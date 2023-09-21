2e5753c425180657272dc9024f0da989ea77eefe
Mike Toutou <miketoutou@gmail.com>
2023-09-21 14:51:49 +0300

    Updates to the other config files plus cleanup of directory

--- fio-configs/benchmarks.sh
+++ fio-configs/benchmarks.sh
@@ -1,1 +1,19 @@
-sudo fio configs.fio > grep 'latency-\|iops-\|throughput-' >  disc.test
+#!/bin/bash
+
+# Check if argument is provided
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <argument>"
+  exit 1
+fi
+
+if [ "$1" = "device" ]; then
+    sudo fio device-configs.fio | grep -f grep_filter > results/disc.test
+elif [ "$1" = "fs" ] ; then
+    sudo fio filesystem-configs.fio | grep -f grep_filter > results/filesystem.test
+elif [ "$1" = "longhorn" ] ; then
+    sudo fio longhorn-configs.fio | grep -f grep_filter > results/longhorn.test
+else
+  echo "Invalid argument: $1"
+  exit 1
+fi
+
