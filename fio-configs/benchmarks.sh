3315ba0543b9ed0810bc80171a5dbed40f15527f
Michael Toutoudakis <toutou@sith2.cluster.ics.forth.gr>
2023-09-21 16:43:43 +0300

    real tests on device plus modification of runtime to config files

--- fio-configs/benchmarks.sh
+++ fio-configs/benchmarks.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+if [ ! -d "results" ]; then
+	mkdir -p "results"
+fi
 
 # Check if argument is provided
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 <argument>"
-  exit 1
+	echo "Usage: $0 <argument>"
+	exit 1
 fi
 
 if [ "$1" = "device" ]; then
-    sudo fio device-configs.fio | grep -f grep_filter > results/disc.test
+	sudo fio device-configs.fio | grep -f grep_filter > results/device.test
 elif [ "$1" = "fs" ] ; then
-    sudo fio filesystem-configs.fio | grep -f grep_filter > results/filesystem.test
+	sudo fio filesystem-configs.fio | grep -f grep_filter > results/filesystem.test
 elif [ "$1" = "longhorn" ] ; then
-    sudo fio longhorn-configs.fio | grep -f grep_filter > results/longhorn.test
+	sudo fio longhorn-configs.fio | grep -f grep_filter > results/longhorn.test
 else
-  echo "Invalid argument: $1"
-  exit 1
+	echo "Invalid argument: $1"
+	exit 1
 fi
 
